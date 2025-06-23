aee6aaa0d96296f491690f18c007dea46cb59e77
Jake Dimitri <dantemitya@gmail.com>
2025-06-23 19:37:12 +0100

    write grocery-list-mngr initial version

---
+++ beginner-projects/grocery-list-mngr/main.py
@@ -0,0 +1,118 @@
+# GROCERY LIST MANAGER (in memory; doesn't save)
+# Add, remove, and sort grocery items (stored in a list of strings of tuples for (item, quantity)).
+# Optionally store prices using a dictionary
+import utils
+from time import sleep
+
+title = "GROCERY LIST MANAGER"
+
+def menu(groceries):
+    """ display the menu with options to choose from """
+    while True:
+        display_list_and_title(title, groceries)
+
+        # user options
+        print("1. Add items to the list")
+        print("2. Remove items from the list")
+        print("3. Clear the entire list")
+        print("Q. Exit")
+        utils.hyphens()
+        
+        # get user choice
+        choice = input("Choice: ")
+        if choice == '1':
+            return 'add'
+        elif choice == '2':
+            return 'remove'
+        elif choice == '3':
+            return 'clear'
+        elif choice.lower() == 'q':
+            display_list_and_title(title, groceries)
+            print("Exiting...")
+            sleep(1.5)
+            quit()
+        else:
+            continue
+
+def display_list_and_title(title, groceries):
+    """ for printing the title and the list within other functions """
+    utils.display(title)
+    display_list(groceries)
+    utils.hyphens()
+
+def parse_choice(menu_exit_code, groceries):
+    """ get the exit code from menu() and then decide which function to execute """
+    if menu_exit_code == 'add':
+        add_menu(groceries)
+    elif menu_exit_code == 'remove':
+        if len(groceries) == 0:
+            display_list_and_title(title, groceries)
+            print("The list is already empty.")
+            sleep(1.5)
+        else:
+            remove_menu(groceries)
+    elif menu_exit_code == 'clear':
+        clear_list(groceries)
+
+def add_menu(groceries):
+    """ the menu for adding items to the grocery list """
+    while True:
+        display_list_and_title(title, groceries) # display title and groceries
+        
+        # add items to the list
+        print("Enter 'Q' when you are finished")
+        item = input("Add to list: ")
+        if item.lower() == 'q':
+            break
+        else:
+            groceries.append(item.capitalize())
+
+def remove_menu(groceries):
+    """ the menu for removing items from the grocery list """
+    while True:
+        display_list_and_title(title, groceries) # display title and groceries
+
+        # remove items from the list
+        print("Enter 'Q' when you are finished")
+        item_index = input("Item to remove from list (index): ")
+        if item_index.lower() == 'q':
+            break
+        elif len(groceries) == 0:
+            print("The list is now empty. Going back to the menu...")
+            sleep(1.5)
+            break
+        else:
+            try:
+                del groceries[int(item_index) - 1]
+            except IndexError: # catch index error so the program doesn't crash
+                continue
+
+def clear_list(groceries):
+    """ clear the grocery list """
+    if len(groceries) == 0:
+        display_list_and_title(title, groceries)
+        print("The list is already empty.")
+        sleep(1.5)
+    else:
+        display_list_and_title(title, groceries)
+        print("Clearing grocery list...")
+        sleep(1.5)
+        groceries.clear()
+
+def display_list(groceries):
+    """ display the grocery list """
+    if len(groceries) == 0:
+        print("Grocery list is currently empty")
+    else:
+        for index, item in enumerate(groceries):
+            print(f"{index + 1}.\t{item}") # output each item in the list line by line
+
+def main():
+    groceries = [] # initialize groceries list
+    while True:
+        menu_exit_code = menu(groceries) # get initial user choice
+        parse_choice(menu_exit_code, groceries) # do one of the 3 possible functions on the list
+
+if __name__ == '__main__':
+    main()
+
