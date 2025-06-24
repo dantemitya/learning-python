a2a17048427f8854f095402b90c0b0f8236f4d91
Jake Dimitri <dantemitya@gmail.com>
2025-06-24 18:26:19 +0100

    Write csv-analyzer

--- beginner-projects/csv-analyzer/main.py
+++ beginner-projects/csv-analyzer/main.py
@@ -1,7 +1,59 @@
-# main template
+# CSV DATA ANALAYZER
+import utils
+
+def get_avgs(data):
+    """ get the averages of all exam scores for each student and return a dict """
+    student_scores = {}
+    for row in data:
+        student_scores[row['Student ID']] = [float(row['Exam 1 Score']), float(row['Exam 2 Score']), float(row['Exam 3 Score'])]
+    for student_id in student_scores:
+        scores = student_scores[student_id]
+        student_scores[student_id] = round((sum(scores) / len(scores)), 1)
+    return student_scores
 
 def main():
-    pass
+    filename = 'data.csv'
+    headers, data = utils.get_csv(filename)
+    utils.show_csv(headers, data)
+    utils.hyphens()
+
+    # get column statistics for exam 1 scores
+    exam_1_scores = utils.get_column_data('Student ID', 'Exam 1 Score', data)
+    print("Exam 1 Statistics")
+    utils.get_max(exam_1_scores)
+    utils.get_min(exam_1_scores)
+    utils.get_avg(exam_1_scores)
+    print()
+
+    # get column statistics for exam 2 scores
+    exam_2_scores = utils.get_column_data('Student ID', 'Exam 2 Score', data)
+    print("Exam 2 Statistics")
+    utils.get_max(exam_2_scores)
+    utils.get_min(exam_2_scores)
+    utils.get_avg(exam_2_scores)
+    print()
+
+    # get column statistics for exam 3 scores
+    exam_3_scores = utils.get_column_data('Student ID', 'Exam 3 Score', data)
+    print("Exam 3 Statistics")
+    utils.get_max(exam_3_scores)
+    utils.get_min(exam_3_scores)
+    utils.get_avg(exam_3_scores)
+
+    # get avgs for all students
+    utils.hyphens()
+    average_scores = get_avgs(data)
+    print("Average scores")
+    print("Student ID\tAverage Score")
+    for key, value in average_scores.items():
+        print(f"{key}\t\t{value}")
+    print()
+
+    # get best performing student
+    print(f"Best perfoming student - {max(average_scores, key=average_scores.get)} ({max(average_scores.values())})")
+
+    # get worst perfoming student
+    print(f"Worst perfoming student - {min(average_scores, key=average_scores.get)} ({min(average_scores.values())})")
 
 if __name__ == '__main__':
     main()
--- beginner-projects/csv-analyzer/utils.py
+++ beginner-projects/csv-analyzer/utils.py
@@ -1,10 +1,11 @@
 # USEFUL METHODS THAT CAN BE REUSED IN VARIOUS SCRIPTS
+import csv
 
 def clear_screen(length=100):
     """ clears the console screen using new lines. default length is 100 """
     print("\n"*length)
 
-def hyphens(length=40):
+def hyphens(length=80):
     """ prints a line of hyphens for formatting. default length is 40 """
     print("-"*length)
 
@@ -13,3 +14,45 @@ def display(title):
     clear_screen()
     print(title.upper())
     hyphens()
+
+def get_csv(filename):
+    """ get the csv and its header name for storing in a variable """
+    with open(filename, newline='') as file:
+        reader = csv.DictReader(file)
+        headers = reader.fieldnames # header names
+        return headers, list(reader) 
+
+def show_csv(headers, data):
+    """ read a csv file """
+    print(headers)
+    hyphens()
+    for row in data:
+        for header in headers:
+            print(row[header], end='\t')
+        print()
+
+def get_column_data(key_column, value_column, data):
+    """ returns specified column data as a dict """
+    column_data = {}
+    for row in data:
+        column_data[row[key_column]] = row[value_column]
+    return column_data
+
+def get_min(column_data):
+    """ gets the smallest value from a column along with the id """
+    min_id = min(column_data, key=lambda k: float(column_data[k]))
+    min_val = float(column_data[min_id])
+    print(f"Lowest - {min_val} (ID: {min_id})")
+
+def get_max(column_data):
+    max_id = max(column_data, key=lambda k: float(column_data[k]))
+    max_val = float(column_data[max_id])
+    print(f"Highest - {max_val} (ID: {max_id})")
+    
+def get_avg(column_data):
+    """ gets the average value from a column """
+    values = [float(value.strip()) for value in column_data.values()]
+    average = sum(values) / len(values)
+    print(f"Average - {average:.1f}")
+
+
